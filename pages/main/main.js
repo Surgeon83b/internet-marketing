dfb667dd5f10796f0e002b7caa5609e9779f8a73
Kanstantsin Piatkevich <surgeon-2@tut.by>
2022-10-17 01:30:31 +0300

    feat: add js forming html file

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,3 +1,14 @@
+const phoneNumber = '+7 (4872) 79-29-10';
+const navItems = [
+    'УВЕЛИЧЕНИЕ ПРОДАЖ',
+    'СОЗДАНИЕ САЙТОВ',
+    'ПОДДЕРЖКА',
+    'БИТРИКС 24',
+    'О КОМПАНИИ',
+    'ОТЗЫВЫ',
+    'КЕЙСЫ',
+    'КОНТАКТЫ'
+]
 let headerSection = document.createElement('div');
 headerSection.classList.add('header-section');
 
@@ -35,8 +46,66 @@ const headerTopLogo = document.createElement('div');
 headerTopLogo.classList.add('logo');
 const headerTopCaption = document.createElement('div');
 headerTopCaption.classList.add('caption');
+headerTopCaption.innerHTML = 'Увеличиваем продажи в Вашем бизнесе!';
 const headerTopContactUs = document.createElement('div');
 headerTopContactUs.classList.add('contact-us');
+
+headerTopWrapper.append(headerTopLogo);
+headerTopWrapper.append(headerTopCaption);
+headerTopWrapper.append(headerTopContactUs);
+
+const headerTopLogoImage = document.createElement('div');
+headerTopLogoImage.classList.add('logo-image');
+const headerTopLogoText = document.createElement('div');
+headerTopLogoText.classList.add('logo-text');
+const headerTopLogoText1 = document.createElement('div');
+headerTopLogoText1.classList.add('logo-text1');
+const headerTopLogoText2 = document.createElement('div');
+headerTopLogoText2.classList.add('logo-text2');
+
+headerTopLogo.append(headerTopLogoImage);
+headerTopLogo.append(headerTopLogoText);
+headerTopLogoText.append(headerTopLogoText1);
+headerTopLogoText.append(headerTopLogoText2);
+
+const phone = document.createElement('span');
+phone.classList.add('phone');
+phone.innerHTML = phoneNumber;
+const callButton = document.createElement('button');
+callButton.classList.add('call-button');
+const callButtonCaption = document.createElement('div');
+callButtonCaption.classList.add('button-caption');
+const phoneIcon = document.createElement('div');
+phoneIcon.classList.add('phone-icon');
+const orderCall = document.createElement('div');
+orderCall.classList.add('order-call');
+orderCall.innerHTML = 'Заказать звонок';
+
+headerTopContactUs.append(phone);
+headerTopContactUs.append(callButton);
+callButton.append(callButtonCaption);
+callButtonCaption.append(phoneIcon);
+callButtonCaption.append(orderCall);
+
+const headerBottomWrapper = document.createElement('div');
+headerBottomWrapper.classList.add('wrapper');
+const navHeader = document.createElement('nav');
+navHeader.classList.add('nav-header');
+const navMenu = document.createElement('ul');
+navMenu.classList.add('nav-menu');
+
+headerBottom.append(headerBottomWrapper);
+headerBottomWrapper.append(navHeader);
+navHeader.append(navMenu);
+for (let i=0; i<8;i++) {
+    let menuItem = document.createElement('li');
+    menuItem.innerHTML = navItems[i];
+    navMenu.append(menuItem);
+}
+let menuItem = document.createElement('li');
+menuItem.classList.add('search');
+navMenu.append(menuItem);
+
 
 
 const section1Caption = document.createElement('div');
